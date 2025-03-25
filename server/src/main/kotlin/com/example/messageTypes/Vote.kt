b28f49408f6a6ae51dbf0b2feb58b22a948f8fbb
chajohnson10 <chajohnson10@gmail.com>
2025-03-25 00:06:06 -0500

    Update Vote.kt

--- server/src/main/kotlin/com/example/messageTypes/Vote.kt
+++ server/src/main/kotlin/com/example/messageTypes/Vote.kt
@@ -5,6 +5,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Vote(
     val userId: String,
+    val receiveUserId: String,
+    val type: PostType,
     val postId: String,
     val upvote: Boolean
 )
+
+@Serializable
+enum class PostType {
+    SUPER,
+    SUB,
+    PROFILE,
+    COMMENT,
+    GAME
+}
