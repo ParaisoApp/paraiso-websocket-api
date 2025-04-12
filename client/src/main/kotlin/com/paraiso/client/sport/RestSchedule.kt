c43c6c19873160a3a1bd2a4f629575ad815d7ec9
chajohnson10 <chajohnson10@gmail.com>
2025-04-11 23:08:53 -0500

    Hold changes here

--- client/src/main/kotlin/com/paraiso/client/sport/RestSchedule.kt
+++ client/src/main/kotlin/com/paraiso/client/sport/RestSchedule.kt
@@ -14,8 +14,33 @@ data class RestEvent(
     val name: String,
     val shortName: String,
     val date: String,
-    val competitions: List<Competition>
+    val competitions: List<RestCompetition>
 )
+
+@Serializable
+data class RestCompetition(
+    val id: String,
+    val venue: Venue,
+    val competitors: List<RestCompetitor>,
+    val status: Status
+)
+
+@Serializable
+data class RestCompetitor(
+    val homeAway: String,
+    val team: RestTeam,
+    val winner: Boolean? = null,
+    val score: RestScore,
+    val statistics: List<TeamYearStats>,
+    val linescores: List<LineScore>? = null,
+    val records: List<Record>
+)
+@Serializable
+data class RestScore(
+    val value: Double,
+    val displayValue: String
+)
+
 @Serializable
 data class RestSeason(
     val year: Int,
