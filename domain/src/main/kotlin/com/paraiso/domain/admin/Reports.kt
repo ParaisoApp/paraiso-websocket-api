d28436ad865e97c300f5148b24f7465a8c3add0c
chajohnson10 <chajohnson10@gmail.com>
2025-08-03 13:42:56 -0500

    Add serializable

--- domain/src/main/kotlin/com/paraiso/domain/admin/Reports.kt
+++ domain/src/main/kotlin/com/paraiso/domain/admin/Reports.kt
@@ -2,20 +2,15 @@ package com.paraiso.domain.admin
 
 import com.paraiso.domain.posts.PostReturn
 import com.paraiso.domain.users.UserResponse
+import kotlinx.serialization.Serializable
 
-data class UserReports(
-    val userReports: Set<UserReport>
-)
-
+@Serializable
 data class UserReport(
     val user: UserResponse,
     val reportedBy: Set<String>
 )
 
-data class PostReports(
-    val postReports: Set<PostReport>
-)
-
+@Serializable
 data class PostReport(
     val post: PostReturn,
     val reportedBy: Set<String>
