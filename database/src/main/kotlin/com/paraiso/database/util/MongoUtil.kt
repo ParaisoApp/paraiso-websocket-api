5f4ebc5ffcc44c24de253b254c8fb52c51a002b9
chajohnson10 <chajohnson10@gmail.com>
2025-08-16 16:01:53 -0500

    Update MongoUtil.kt

--- database/src/main/kotlin/com/paraiso/database/util/MongoUtil.kt
+++ database/src/main/kotlin/com/paraiso/database/util/MongoUtil.kt
@@ -5,7 +5,7 @@ import org.bson.Document
 import org.bson.conversions.Bson
 import kotlin.reflect.KProperty1
 
-suspend fun <T : Any> fieldsEq(
+fun <T : Any> fieldsEq(
     field1: KProperty1<T, *>,
     field2: KProperty1<T, *>
 ): Bson = Filters.expr(Document("\$eq", listOf("\$${field1.name}", "\$${field2.name}")))
