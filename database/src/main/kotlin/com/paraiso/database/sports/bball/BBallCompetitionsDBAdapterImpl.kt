b3f251a0bd2f28aa97ce07640ab3a5e5beb920f5
chajohnson10 <chajohnson10@gmail.com>
2025-08-16 22:16:31 -0500

    competitions

---
+++ database/src/main/kotlin/com/paraiso/database/sports/bball/BBallCompetitionsDBAdapterImpl.kt
@@ -0,0 +1,19 @@
+package com.paraiso.database.sports.bball
+
+import com.mongodb.client.model.Filters
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import com.paraiso.domain.sport.adapters.bball.BBallCompetitionsDBAdapter
+import com.paraiso.domain.sport.adapters.bball.BBallSchedulesDBAdapter
+import com.paraiso.domain.sport.data.Competition
+import com.paraiso.domain.sport.data.Schedule
+import kotlinx.coroutines.flow.firstOrNull
+
+class BBallCompetitionsDBAdapterImpl(database: MongoDatabase) : BBallCompetitionsDBAdapter {
+    private val collection = database.getCollection("bballCompetitions", Competition::class.java)
+
+    suspend fun findById(id: String) =
+        collection.find(Filters.eq(Competition::id.name, id)).firstOrNull()
+
+    suspend fun save(competitions: List<Competition>) =
+        collection.insertMany(competitions)
+}
---
+++ domain/src/main/kotlin/com/paraiso/domain/sport/adapters/bball/BBallCompetitionsDBAdapter.kt
@@ -0,0 +1,3 @@
+package com.paraiso.domain.sport.adapters.bball
+
+interface BBallCompetitionsDBAdapter
