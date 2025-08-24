86e626841bd1f477654e697421e1eff753452927
chajohnson10 <chajohnson10@gmail.com>
2025-08-24 14:09:16 -0500

    Setup team leaders

--- database/src/main/kotlin/com/paraiso/database/sports/LeadersDBImpl.kt
+++ database/src/main/kotlin/com/paraiso/database/sports/LeadersDBImpl.kt
@@ -38,6 +38,7 @@ class LeadersDBImpl(database: MongoDatabase) : LeadersDB {
     collection.find(
         and(
             eq(StatLeaders::sport.name, sport),
+            eq(StatLeaders::teamId.name, teamId),
             eq(StatLeaders::season.name, season),
             eq(StatLeaders::type.name, type),
         )
